fc894310e8b970ba59788e69b19eaa89a633bc0f
alphadlplus <82231865+alphadlplus@users.noreply.github.com>
2022-02-04 17:41:55 +0330

    Add files via upload

---
+++ nim.php
@@ -0,0 +1,9 @@
+<?
+header("Content-Type: video/mp4");
+//header('Content-Disposition: inline; filename="alphadlbot"');
+
+$url = "http://dl1.alphadl.xyz/Movies/1920/The.Cabinet.Of.DrCaligari/The.Cabinet.Of.DrCaligari.1920.1080p.BluRay.x264.AAC5.1-YTS.AlphaDL.mp4";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+$audio = curl_exec($ch);
+return $audio;
